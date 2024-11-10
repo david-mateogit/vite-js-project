1fc1a339775f7c4c50613babd087511fa1fee900
David Mateo <davidmateo@outlook.com>
2024-11-10 10:45:08 -0500

    Remove 404.html and related plugins from Vite configuration; simplify package.json dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,9 @@
-import path from "path";
 import { defineConfig } from "vite";
-import Pages from "vite-plugin-pages";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   root: "src",
   build: {
     outDir: "../dist",
   },
-  plugins: [
-    Pages(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: path.resolve(__dirname, "src/404.html"),
-          dest: ".",
-        },
-      ],
-    }),
-  ],
+  base: "/",
 });
